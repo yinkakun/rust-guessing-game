2182cd9753c8b39bc2e23ab090c60db9c6a08749
yinkakun <yinkakun@gmail.com>
2023-01-28 10:44:47 +0100

    implement guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number game 👀");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+        println!("Guess a number 🐝");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Your guess isn't a number 🙄");
+                continue;
+            }
+        };
+
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("The number you guessed is too small"),
+            Ordering::Greater => println!("The number you guessed is too big"),
+            Ordering::Equal => {
+                println!("You win! 🤯");
+                break;
+            }
+        };
+    }
 }
